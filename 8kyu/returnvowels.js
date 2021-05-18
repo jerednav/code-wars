7797c87eb81d2e62036464632e59a1bf655456f9
jerednav <80506833+jerednav@users.noreply.github.com>
2021-05-17 22:53:27 -0700

    Delete 8kyu directory

--- 8kyu/returnvowels.js
+++
@@ -1,8 +0,0 @@
-// Return the number (count) of vowels in the given string. 
-// We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or space.
-
-function getCount(str) {
-    var vowelsCount = 0;
-    const count = str.match(/[aeiou]/gi).length;
-    return count;
-  }
--- 8kyu/reversedstring.js
+++
@@ -1,6 +0,0 @@
-// Complete the solution so that it reverses the string pass into it
-
-
-function solution(str){
-    return str.split('').reverse().join('')
-    }
