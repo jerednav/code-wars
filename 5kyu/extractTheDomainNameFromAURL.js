fc7cb82c2484fe379e30a80d3116ac552966dbf5
jerednav <80506833+jerednav@users.noreply.github.com>
2021-07-21 07:51:56 -0700

    Update extractTheDomainNameFromAURL.js

--- 5kyu/extractTheDomainNameFromAURL.js
+++ 5kyu/extractTheDomainNameFromAURL.js
@@ -8,3 +8,7 @@
 function domainName(url){
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.').shift()
 }
+
+//replace all possibilities (3), with blank
+//split the new string from the period
+//new array is ['cnet','.com'], shift then removes last element of array, so its left with cnet
