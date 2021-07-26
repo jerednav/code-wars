040799d66d433f718470a59883e5ff523e786a9c
jerednav <80506833+jerednav@users.noreply.github.com>
2021-07-25 23:07:21 -0700

    Update highestScoringWord.js

--- 6kyu/highestScoringWord.js
+++ 6kyu/highestScoringWord.js
@@ -9,10 +9,19 @@
 // All letters will be lowercase and all inputs will be valid.
 
 function high(str){
+  //create values for each value of the alphabet
   const alphabet = ' abcdefghijklmnopqrstuvwxyz'
+  //create score for words 
+    //split string into an array. 
+    //reduce letters from array into a number, values are from the alphabet's index
   const wordScore = word => word.split('').reduce((a,c)=> a + alphabet.indexOf(c), 0)
+  
+  //create highest word and count value
   let highestWord = ''
   let highestCount = 0
+  
+  //split string of words
+    //for each word, find which has the highest word count
   str.split(' ').forEach(w=>{
     const scoreCheck = wordScore(w)
     if (scoreCheck > highestCount) {
