d89b943381a30cf8b45e646edd3a94710f80b4f5
jerednav <80506833+jerednav@users.noreply.github.com>
2021-07-27 13:08:12 -0700

    Update numbersToLetters.js

--- 7Kyu/numbersToLetters.js
+++ 7Kyu/numbersToLetters.js
@@ -7,3 +7,18 @@
 
 const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
 const switcher = x => x.map(item => alpha[item]).join('')
+
+//Parameters
+// array of numbers (in strings)
+
+// Return
+// return a string
+
+// Example
+// Test.assertEquals(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+
+// Pseudocode
+// 1. correspond alphabet backwards to values, then connecting it to a variable
+// 2. use map (using map because you need to create a new array)
+// 3. going through each numbers in strings and corresponding it to the alphabet
+// 4. join those corresponding letters, to create a string
