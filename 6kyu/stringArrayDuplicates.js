3fce665330632bc35425e90dfca9e7b3156aab1d
jerednav <80506833+jerednav@users.noreply.github.com>
2021-08-04 08:34:51 -0700

    Update stringArrayDuplicates.js

--- 6kyu/stringArrayDuplicates.js
+++ 6kyu/stringArrayDuplicates.js
@@ -16,23 +16,23 @@ function dup(arr) {
 };
 
 //Parameters
-  array of strings
+//   array of strings
   
-Return
-  array of strings with no duplicate letters
+// Return
+//   array of strings with no duplicate letters
 
-Pseudocode
-   1. map through the array for each string
-   2. once mapped, split the string into letters
-   3. filter through each letter
-   4. 'a', 'l', 'l', 'o', 't', 't', 'e', 'e'  "allottee"
-   5. example: .filter 
-      (a, 0) => a !== str[0-1]
-      (l, 1) => l ! == str[1-1]
-      (l, 2) => l !== str [2-1]
-      (o, 3) => o !== str [3-1]
-      (t, 4) => t !== str [4-1]
-      (t, 5) => t !== str [5-1]
-      (e, 6) => t !== str [6-1]
-      (e, 7) => t !== str [7-1]
-    6. Then join the filtered letters into a string.
+// Pseudocode
+//    1. map through the array for each string
+//    2. once mapped, split the string into letters
+//    3. filter through each letter
+//    4. 'a', 'l', 'l', 'o', 't', 't', 'e', 'e'  "allottee"
+//    5. example: .filter 
+//       (a, 0) => a !== str[0-1]
+//       (l, 1) => l ! == str[1-1]
+//       (l, 2) => l !== str [2-1]
+//       (o, 3) => o !== str [3-1]
+//       (t, 4) => t !== str [4-1]
+//       (t, 5) => t !== str [5-1]
+//       (e, 6) => t !== str [6-1]
+//       (e, 7) => t !== str [7-1]
+//     6. Then join the filtered letters into a string.
